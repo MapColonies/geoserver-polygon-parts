b118dd9ec543afc85af6fe69fa19070a3bdd1cd9
Ronit Kissis <92728393+RonitKissis@users.noreply.github.com>
2024-11-14 15:12:33 +0200

    fix: get LayerName as original productId and Type from catalog(MAPCO-5241) (#10)
    * feat: get LayerName as original productId and Type from catalog
    
    * fix: pr changes
    
    * fix: default

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ import { strictEqual as assertEqual, ok as assertOk } from 'assert';
 import env from 'env-var';
 import 'dotenv/config';
 import jsLogger from '@map-colonies/js-logger';
+import { ProductType } from '@map-colonies/mc-model-types';
 
 // *******************************************************************
 // *************** initialization of environ variables ***************
@@ -12,26 +13,28 @@ const logger = jsLogger.default({
   prettyPrint: env.get('LOG_PRETTY').default('false').asBool(),
 });
 
-const GEOSERVER_BASE_URL = env.get('GEOSERVER_BASE_URL').asString();
+const GEOSERVER_BASE_URL = env.get('GEOSERVER_BASE_URL').default('http://localhost:8080/geoserver').asString();
 
-const GEOSERVER_API_BASE_URL = env.get('GEOSERVER_API_BASE_URL').asString();
+const GEOSERVER_API_BASE_URL = env.get('GEOSERVER_API_BASE_URL').default('http://localhost:8081').asString();
+const CATALOG_MANAGER_SERVICE_URL = env.get('CATALOG_MANAGER_SERVICE_URL').default('http://localhost:8082').asString();
 
-const WORKSPACE_NAME = env.get('WORKSPACE_NAME').asString();
-const DATASTORE_NAME = env.get('DATASTORE_NAME').asString();
+const WORKSPACE_NAME = env.get('WORKSPACE_NAME').default('polygonParts').asString();
+const DATASTORE_NAME = env.get('DATASTORE_NAME').default('polygonParts').asString();
 
-const FEATURE_TYPES_STRINGS_BLACK_LIST = env.get('FEATURE_TYPES_STRINGS_BLACK_LIST').asJson();
-const FEATURE_TYPES_REGEX_BLACK_LIST = env.get('FEATURE_TYPES_REGEX_BLACK_LIST').asJson();
+const FEATURE_TYPES_STRINGS_BLACK_LIST = env.get('FEATURE_TYPES_STRINGS_BLACK_LIST').default(['*_parts']).asJson();
+const FEATURE_TYPES_REGEX_BLACK_LIST = env.get('FEATURE_TYPES_REGEX_BLACK_LIST').default(['migrations','parts','polygon_parts','test_view']).asJson();
 
 const WORKSPACE_API_URL = `${GEOSERVER_API_BASE_URL}/workspaces`;
 const DATA_STORE_API_URL = `${GEOSERVER_API_BASE_URL}/dataStores/${WORKSPACE_NAME}`;
 const FEATURE_TYPES_API_URL = `${GEOSERVER_API_BASE_URL}/featureTypes/${WORKSPACE_NAME}/${DATASTORE_NAME}`;
+const CATALOG_MANAGER_FIND_URL = `${CATALOG_MANAGER_SERVICE_URL}/records/find`;
 
 const GLOBAL_WFS_SETTING_API_URL = `${GEOSERVER_API_BASE_URL}/services/wfs/settings`;
 
 
 // *******************GEOSERVER INITIALIZATION************************************************
 
-// Loop until validate geoserver is up
+//Loop until validate geoserver is up
 await checkGeoserverIsUp();
 
 //set wfs mode
@@ -171,28 +174,29 @@ async function createDataStore() {
  * compare get list configured and get available - if create feature types of all that are available and not configured
  */
 async function checkFeatureTypes() {
-  // Extract layer names from both arrays
-  const availableNames = await getAvailableFeatureTypes();
 
-  if (availableNames.length === 0) {
+  const availableNames = await getAvailableFeatureTypes();
+  const mappedLayerNames= await mapNativeNameToLayerName(availableNames);
+
+  if (mappedLayerNames.length === 0) {
     logger.info(' There are no layers to publish! ');
   } else {
-    const postRequests = availableNames.map(async (name) => {
+    const postRequests = mappedLayerNames.map(async (entity) => {
       try {
         const response = await zx.fetch(FEATURE_TYPES_API_URL, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ nativeName: name }),
+          body: JSON.stringify({ nativeName: entity.nativeName, name: entity.layerName }),
         });
         if (!response.ok) {
-          throw new Error(`Failed to POST for ${name}: ${response.status} ${response.statusText}`);
+          throw new Error(`Failed to POST for table:${entity.nativeName} with layerName: ${entity.name}: ${response.status} ${response.statusText}`);
         }
-        logger.info(`Successfully posted for ${name}`);
+        logger.info(`Successfully posted for table:${entity.nativeName} with layerName: ${entity.name}`);
       } catch (error) {
         // Log detailed error message for the failed request
-        logger.error(`Error posting for ${name}:`, error);
+        logger.error(`Error posting for table:${entity.nativeName} with layerName: ${entity.name}:`, error);
         throw error; // Re-throw the error to ensure it is caught by Promise.all
       }
     });
@@ -257,6 +261,68 @@ async function getConfiguredFeatureTypes() {
   await zx.sleep(1000);
   return configuredNames;
 }
+
+async function mapNativeNameToLayerName(availableNames) {
+  const configuredLayers= await getConfiguredFeatureTypes();
+  const layersMapping = await Promise.all(availableNames.map(async (nativeName) => {
+    const { productId, productType } = splitProductIdAndType(nativeName);
+    try {
+      const response = await zx.fetch(CATALOG_MANAGER_FIND_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ metadata: { productId, productType } }),
+      });
+      const layerDetails = await response.json();
+      if (layerDetails.length !== 1) {
+        throw new Error(`Expected exactly one result for ${nativeName}, but got ${layerDetails.length}`);
+      }
+      const fetchedProductId = layerDetails[0].metadata.productId;
+      const fetchedProductType = layerDetails[0].metadata.productType;
+
+      const layerName = `${fetchedProductId}-${fetchedProductType}`;
+      
+      /* getAvailable returns the tableNames. 
+      Due to the fact that we are publishing the features in a different name from 
+      the tableName, the available returns  some already published layers
+      so we want to get the configuredLayers and check that the layer name is not in it*/
+      if(!configuredLayers.includes(layerName)){
+        return { nativeName, layerName };
+      }
+      return undefined;
+
+    } catch (error) {
+      logger.error(`Error processing ${nativeName}: ${error.message}`);
+      return undefined;
+    }
+  }));
+  //filter out undefined values returned from the mapping
+  return layersMapping.filter((result) => result);
+}
+
+
+function splitProductIdAndType(name) {
+  const lastUnderscoreIndex = name.lastIndexOf('_');
+
+  const productId = name.slice(0, lastUnderscoreIndex);
+  const productType = findProductType(name.slice(lastUnderscoreIndex + 1));
+  
+  return { productId, productType };
+}
+
+function findProductType(input) {
+  // Normalize the input by converting it to lowercase
+  const formattedInput = input.toLowerCase();
+
+  // Loop through the enum values
+  return Object.values(ProductType).find(value => {
+      // Normalize the enum value by converting it to lowercase and removing underscores
+      const normalizedEnumValue = value.toLowerCase().replace(/_/g, '');
+      return normalizedEnumValue === formattedInput;
+  });
+}
+
 
 function printAvocado() {
   console.log(`
